3cac42d2a2684d76fba1810e3ec31d475d505cf2
chithanhtrieu4-commits <chithanhtrieu4@gmail.com>
2026-09-19 14:32:53 +0700

    Create setting.php

---
+++ setting.php
@@ -0,0 +1,18 @@
+<?php
+// Database hostname
+define('BYTEBEAT_DBHOST', '');
+// Database username
+define('BYTEBEAT_DBUSERNAME', '');
+// Database password
+define('BYTEBEAT_DBPASSWORD', '');
+// Database name
+define('BYTEBEAT_DBNAME', '');
+// Timezone
+define('BYTEBEAT_TIMEZONE', 'UTC');
+// "Make database" button
+define('BYTEBEAT_DBMAKE', false);
+
+// Password for admin login
+$bytebeat_admins = array(
+    'admin' => 'YourPasswordHere'
+);
